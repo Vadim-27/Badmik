8ab7720d436d63ab7f792656e78a3df58def8b29
Roman Mygaluk <fisher_6@outlook.com>
2025-07-24 20:59:12 +0300

    Web login

--- src/BadmintonApp.Web/Program.cs
+++ src/BadmintonApp.Web/Program.cs
@@ -1,3 +1,6 @@
+using BadmintonApp.Web.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +11,35 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseStaticFiles();
 app.UseRouting();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var token = context.Request.Cookies[AuthConstants.AuthCookieName];
+                if (!string.IsNullOrEmpty(token))
+                {
+                    context.Token = token;
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
 app.MapRazorPages();
+app.UseAuthorization();
 app.MapFallbackToFile("/index.html");
+app.Use(async (context, next) =>
+{
+    await next();
+
+    // only for HTML pages (not API)
+    if (context.Response.StatusCode == 401 &&
+        context.Request.Path.StartsWithSegments("/"))
+    {
+        context.Response.Redirect("/login");
+    }
+});
+builder.Services.AddHttpClient();
 app.Run();
