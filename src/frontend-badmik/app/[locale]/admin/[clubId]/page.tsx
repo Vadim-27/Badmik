4545922af55b07b128ee2a3c06aab9de93cc021f
Vadym <vadim.bulanyi@gmail.com>
2025-12-03 11:17:13 +0100

    again club details

--- src/frontend-badmik/app/[locale]/admin/[clubId]/page.tsx
+++ src/frontend-badmik/app/[locale]/admin/[clubId]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
 import { clubs } from '@/data/clubs';
-import { ClubDetails } from '@/app/components/shared/Clubs/ClubDetails/ClubDetails';
+// import { ClubDetails } from '@/app/components/shared/Clubs/ClubDetails/ClubDetails';
 // import { ClubPageHeader } from '@/app/components/shared/Clubs/ClubPageHeader/ClubPageHeader';
 import {Link} from '@/i18n/navigation';
 
@@ -38,7 +38,8 @@ export default  async function ClubAdminPage({
     <main className="p-6 bg-gray-100 min-h-screen">
       {/* <div className="bg-gray-100 h-100vh"> */}
       {/* <ClubPageHeader club={club} /> */}
-      <ClubDetails club={club}/>
+      {/* <ClubDetails club={club}/> */}
+      <h1 className="text-2xl font-bold mb-4">Admin Panel for {club.name}</h1>
      
     </main>
   );
